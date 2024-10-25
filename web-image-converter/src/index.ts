9b6c6cd4260d2b916c9803f22d87006ed8383f72
toothlessdev <toothless042@gmail.com>
2024-10-25 20:59:10 +0900

    feature: Implements Image Converter

---
+++ web-image-converter/src/index.ts
@@ -0,0 +1,24 @@
+import { ImageConverter } from "./ImageConverter";
+
+class WEBPImageConverter extends ImageConverter {
+    constructor(options: ImageConverterOptions) {
+        super(options);
+    }
+    public async convert() {
+        return super.convert(ImageType.WEBP);
+    }
+}
+
+class PNGImageConverter extends ImageConverter {
+    constructor(options: ImageConverterOptions) {
+        super(options);
+    }
+    public async convert() {
+        return super.convert(ImageType.PNG);
+    }
+}
+
+export const imageConverter = {
+    webp: WEBPImageConverter,
+    png: PNGImageConverter,
+};
